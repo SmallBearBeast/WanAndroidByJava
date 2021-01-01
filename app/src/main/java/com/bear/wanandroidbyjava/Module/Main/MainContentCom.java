a9901720e669f4ac78236be0fc97a4a1ca3fcc4d
hugo.wu <hugo.wu@ringcentral.com>
2021-01-01 22:32:02 +0800

    Fix: TAB_HOME should be 0

--- app/src/main/java/com/bear/wanandroidbyjava/Module/Main/MainContentCom.java
+++ app/src/main/java/com/bear/wanandroidbyjava/Module/Main/MainContentCom.java
@@ -7,7 +7,7 @@ import com.bear.libcomponent.ViewComponent;
 import com.bear.wanandroidbyjava.R;
 
 public class MainContentCom extends ViewComponent<ComponentAct> {
-    public static final int TAB_HOME = 1;
+    public static final int TAB_HOME = 0;
     public static final int TAB_SYSTEM = TAB_HOME + 1;
     public static final int TAB_PUBLIC = TAB_SYSTEM + 1;
     public static final int TAB_PROJECT = TAB_PUBLIC + 1;
