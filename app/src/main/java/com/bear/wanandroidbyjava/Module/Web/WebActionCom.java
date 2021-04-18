366cd3c591088f7362f079aacf2a613965765707
hugo.wu <hugo.wu@ringcentral.com>
2021-04-18 14:13:39 +0800

    Fix: RemoveBrowserSelectWindow function

--- app/src/main/java/com/bear/wanandroidbyjava/Module/Web/WebActionCom.java
+++ app/src/main/java/com/bear/wanandroidbyjava/Module/Web/WebActionCom.java
@@ -1,25 +1,17 @@
 package com.bear.wanandroidbyjava.Module.Web;
 
-import android.view.Gravity;
 import android.view.View;
-
-import androidx.constraintlayout.widget.ConstraintLayout;
-
 import com.bear.libcomponent.ComponentAct;
 import com.bear.libcomponent.ComponentService;
 import com.bear.libcomponent.ViewComponent;
 import com.bear.wanandroidbyjava.R;
-import com.example.libbase.Util.DensityUtil;
 
-public class WebActionCom extends ViewComponent<ComponentAct> implements View.OnClickListener{
+public class WebActionCom extends ViewComponent<ComponentAct> implements View.OnClickListener {
     private static final String TAG = WebAct.TAG + "-WebActionCom";
-    private BrowserSelectWindow mBrowserSelectWindow;
 
-    private ConstraintLayout mClActionContainer;
     @Override
     protected void onCreate() {
-        mClActionContainer = findViewById(R.id.cl_action_container);
-        clickListener(this, R.id.iv_cancel, R.id.iv_browser_type, R.id.iv_more, R.id.iv_forward);
+        clickListener(this, R.id.iv_cancel, R.id.iv_more, R.id.iv_forward);
     }
 
     @Override
@@ -27,19 +19,6 @@ public class WebActionCom extends ViewComponent<ComponentAct> implements View.On
         switch (v.getId()) {
             case R.id.iv_cancel:
                 getDependence().finish();
-                break;
-
-            case R.id.iv_browser_type:
-                if (mBrowserSelectWindow == null) {
-                    mBrowserSelectWindow = new BrowserSelectWindow(getDependence(), DensityUtil.getScreenWidth(), DensityUtil.dp2Px(45));
-                }
-                mBrowserSelectWindow.showAtLocation(mClActionContainer, Gravity.BOTTOM, 0, 0);
-                mBrowserSelectWindow.setBrowserSelectListener(new BrowserSelectWindow.BrowserSelectListener() {
-                    @Override
-                    public void onBrowserType(int browserType) {
-                        ComponentService.get().getComponent(WebInputCom.class).setSearchType(browserType);
-                    }
-                });
                 break;
 
             case R.id.iv_more:
